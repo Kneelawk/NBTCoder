eebb28763734e6ca39269216df62358641f0c616
Kneelawk <jedidiah.pommert@gmail.com>
2018-11-18 08:25:13 -0800

    Add string parse source

--- src/main/java/com/github/kneelawk/nbtcoder/NBTLanguageParser.java
+++ src/main/java/com/github/kneelawk/nbtcoder/NBTLanguageParser.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.antlr.v4.runtime.BufferedTokenStream;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
@@ -11,8 +12,16 @@ import com.github.kneelawk.nbt.Tag;
 import com.github.kneelawk.nbtcoder.NBTCoderParser.NbtFileContext;
 
 public class NBTLanguageParser {
+	public Tag parse(String str) throws IOException {
+		return parse(CharStreams.fromString(str));
+	}
+
 	public Tag parse(InputStream is) throws IOException {
-		NBTCoderLexer lex = new NBTCoderLexer(CharStreams.fromStream(is));
+		return parse(CharStreams.fromStream(is));
+	}
+
+	public Tag parse(CharStream cs) throws IOException {
+		NBTCoderLexer lex = new NBTCoderLexer(cs);
 		NBTCoderParser parser = new NBTCoderParser(new BufferedTokenStream(lex));
 		NBTCoderBuilderListener builder = new NBTCoderBuilderListener();
 		NbtFileContext tree = parser.nbtFile();
