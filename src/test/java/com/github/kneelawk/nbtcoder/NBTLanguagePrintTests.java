1af54733f6e66cbe4ebfd2b0ab93b77b35a2e4d6
Kneelawk <jedidiah.pommert@gmail.com>
2018-11-18 10:41:11 -0800

    Add TagShort tests

--- src/test/java/com/github/kneelawk/nbtcoder/NBTLanguagePrintTests.java
+++ src/test/java/com/github/kneelawk/nbtcoder/NBTLanguagePrintTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import com.github.kneelawk.nbt.TagByte;
+import com.github.kneelawk.nbt.TagShort;
 
 public class NBTLanguagePrintTests {
 	@ParameterizedTest
@@ -13,6 +14,17 @@ public class NBTLanguagePrintTests {
 	public void printByte(boolean prettyPrint, boolean printRootName, String name, byte value, String expected) {
 		NBTLanguagePrinter printer = new NBTLanguagePrinter.Builder(prettyPrint, printRootName).build();
 		TagByte b = new TagByte(name, value);
+
+		String out = printer.print(b);
+
+		assertEquals(out, expected);
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = { "printShort.csv" })
+	public void printShort(boolean prettyPrint, boolean printRootName, String name, short value, String expected) {
+		NBTLanguagePrinter printer = new NBTLanguagePrinter.Builder(prettyPrint, printRootName).build();
+		TagShort b = new TagShort(name, value);
 
 		String out = printer.print(b);
 
