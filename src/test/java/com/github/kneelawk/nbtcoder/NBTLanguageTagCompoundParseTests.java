2d647297b1c1bebd5fd6c02ceae08deb3601b977
Kneelawk <jedidiah.pommert@gmail.com>
2018-11-18 17:47:47 -0800

    Add tests for TagCompound

---
+++ src/test/java/com/github/kneelawk/nbtcoder/NBTLanguageTagCompoundParseTests.java
@@ -0,0 +1,36 @@
+package com.github.kneelawk.nbtcoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import com.github.kneelawk.nbt.Tag;
+import com.github.kneelawk.nbt.TagCompound;
+
+public class NBTLanguageTagCompoundParseTests {
+	@ParameterizedTest
+	@CsvFileSource(resources = { "prettyPrintTagCompoundWithName.csv", "simplePrintTagCompoundWithName.csv" })
+	public void parseTagCompoundWithName(String name, @ConvertWith(TagListArgumentConverter.class) List<Tag> data,
+			String input) throws IOException {
+		NBTLanguageParser parser = new NBTLanguageParser();
+		TagCompound tag = (TagCompound) parser.parse(input);
+
+		assertEquals(name, tag.getName());
+		assertIterableEquals(data, tag.tags());
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = { "prettyPrintTagCompoundWithoutName.csv", "simplePrintTagCompoundWithoutName.csv" })
+	public void parseTagCompoundWithoutName(String name, @ConvertWith(TagListArgumentConverter.class) List<Tag> data,
+			String input) throws IOException {
+		NBTLanguageParser parser = new NBTLanguageParser();
+		TagCompound tag = (TagCompound) parser.parse(input);
+
+		assertIterableEquals(data, tag.tags());
+	}
+}
