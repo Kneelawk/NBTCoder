aad5b79932cff26b3e6e5756dad71490bc5e1b5a
Kneelawk <jedidiah.pommert@gmail.com>
2018-11-18 16:52:33 -0800

    Add tests for TagList

---
+++ src/test/java/com/github/kneelawk/nbtcoder/NBTLanguageTagListParseTests.java
@@ -0,0 +1,38 @@
+package com.github.kneelawk.nbtcoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import com.github.kneelawk.nbt.Tag;
+import com.github.kneelawk.nbt.TagList;
+
+public class NBTLanguageTagListParseTests {
+	@ParameterizedTest
+	@CsvFileSource(resources = { "prettyPrintTagListWithName.csv", "simplePrintTagListWithName.csv" })
+	public void parseTagListWithName(String name, @ConvertWith(TagListArgumentConverter.class) List<Tag> data,
+			String input) throws IOException {
+		NBTLanguageParser parser = new NBTLanguageParser();
+		@SuppressWarnings("unchecked")
+		TagList<Tag> tag = (TagList<Tag>) parser.parse(input);
+
+		assertEquals(name, tag.getName());
+		assertEquals(data, tag.getElements());
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = { "prettyPrintTagListWithoutName.csv", "simplePrintTagListWithoutName.csv" })
+	public void parseTagListWithoutName(String name, @ConvertWith(TagListArgumentConverter.class) List<Tag> data,
+			String input) throws IOException {
+		NBTLanguageParser parser = new NBTLanguageParser();
+		@SuppressWarnings("unchecked")
+		TagList<Tag> tag = (TagList<Tag>) parser.parse(input);
+
+		assertEquals(data, tag.getElements());
+	}
+}
---
+++ src/test/java/com/github/kneelawk/nbtcoder/TagListArgumentConverter.java
@@ -0,0 +1,38 @@
+package com.github.kneelawk.nbtcoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+
+import com.github.kneelawk.nbt.Tag;
+import com.google.common.collect.ImmutableList;
+
+public class TagListArgumentConverter extends SimpleArgumentConverter {
+	@Override
+	protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+		assertTrue(targetType.isAssignableFrom(List.class));
+
+		String input = (String) source;
+		if (input.isEmpty()) {
+			return ImmutableList.<Tag>of();
+		}
+
+		NBTLanguageParser parser = new NBTLanguageParser();
+
+		return Arrays.stream(input.split("\\|")).map(str -> {
+			try {
+				return parser.parse(str);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}).collect(Collectors.toList());
+	}
+
+}
