31976506c9cf0ec48b004f20505f8c6e162c0475
Kneelawk <jedidiah.pommert@gmail.com>
2018-12-23 23:03:17 -0800

    Begin using RegionFileIO

--- src/main/java/com/github/kneelawk/nbtcoder/RegionTest.java
+++ src/main/java/com/github/kneelawk/nbtcoder/RegionTest.java
@@ -1,17 +1,64 @@
 package com.github.kneelawk.nbtcoder;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.util.List;
 
 import com.github.kneelawk.nbt.DefaultTagFactory;
 import com.github.kneelawk.nbt.TagFactory;
 import com.github.kneelawk.nbtlanguage.NBTLanguagePrinter;
+import com.github.kneelawk.region.Chunk;
+import com.github.kneelawk.region.Partition;
+import com.github.kneelawk.region.PartitionType;
+import com.github.kneelawk.region.RegionFileIO;
+import com.github.kneelawk.region.RegionValues;
 
 public class RegionTest {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		TagFactory factory = new DefaultTagFactory();
 		NBTLanguagePrinter printer = new NBTLanguagePrinter.Builder().build();
+		PrintStream out = new PrintStream(new FileOutputStream("../r.-1.-2.mca.txt"));
+		out.println("!file");
+		out.println("name=r.-1.-2.mca");
+		out.println("type=region");
+
+		System.out.println("Loading...");
+
+		List<Partition> partitions = RegionFileIO.readRegionFile(new FileInputStream("../r.-1.-2.mca"), factory);
+
+		System.out.println("Printing...");
+
+		int size = partitions.size();
+		for (int i = 0; i < size; i++) {
+			Partition part = partitions.get(i);
+
+			out.println("!partition");
+
+			PartitionType type = part.getType();
+
+			if (type == PartitionType.EMPTY) {
+				out.println("type=empty");
+			} else if (type == PartitionType.RESERVED) {
+				out.println("type=reserved");
+			} else {
+				Chunk c = part.getChunk();
+				out.println("type=chunk");
+				out.println("compression="
+						+ (c.getCompressionType() == RegionValues.DEFLATE_COMPRESSION ? "deflate" : "gzip"));
+				out.println("x=" + c.getX());
+				out.println("z=" + c.getZ());
+				out.println("!data");
+				out.println(printer.print(c.readData(factory)));
+			}
+			System.out.println("Partition " + i + " / " + size);
+		}
+
+		System.out.println("Done.");
+		out.close();
 	}
 
 }
--- src/main/java/com/github/kneelawk/region/RegionFileIO.java
+++ src/main/java/com/github/kneelawk/region/RegionFileIO.java
@@ -2,7 +2,9 @@ package com.github.kneelawk.region;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -10,6 +12,10 @@ import java.util.TreeMap;
 import com.github.kneelawk.nbt.TagFactory;
 
 public class RegionFileIO {
+	public static List<Partition> readRegionFile(InputStream is, TagFactory factory) throws IOException {
+		return readRegionFile(new DataInputStream(is), factory);
+	}
+
 	public static List<Partition> readRegionFile(DataInputStream input, TagFactory factory) throws IOException {
 		// keys are stored in ascending order
 		Map<Integer, ChunkLoc> offsetMap = new TreeMap<>();
@@ -21,10 +27,47 @@ public class RegionFileIO {
 				offsetMap.put(loc.getSectorNumber(), loc);
 			}
 		}
-		
-		
+
+		int[] timestamps = new int[RegionValues.INTS_PER_SECTOR];
+		for (int i = 0; i < RegionValues.INTS_PER_SECTOR; i++) {
+			timestamps[i] = input.readInt();
+		}
 
 		List<Partition> partitions = new ArrayList<>();
+
+		int sectorsRead = 2;
+		Iterator<Integer> it = offsetMap.keySet().iterator();
+		while (it.hasNext()) {
+			int sectorNum = it.next();
+
+			// skip to the correct sector
+			if (sectorsRead < sectorNum) {
+				input.skipBytes((sectorNum - sectorsRead) * RegionValues.BYTES_PER_SECTOR);
+				partitions.add(new Partition(PartitionType.EMPTY, sectorsRead, sectorNum - sectorsRead));
+				sectorsRead = sectorNum;
+			} else if (sectorsRead > sectorNum) {
+				throw new IOException("Skipped sectors");
+			}
+
+			ChunkLoc loc = offsetMap.get(sectorNum);
+
+			int length = input.readInt();
+			byte type = input.readByte();
+
+			byte[] data = new byte[length];
+			input.readFully(data);
+
+			Chunk chunk = new Chunk(type, loc.getX(), loc.getZ(), data, timestamps[sectorNum]);
+
+			// Align to the sector borders.
+			// The extra -5 is because of the 4 bytes of length data and 1 byte of
+			// compression data read beforehand
+			input.skipBytes(loc.getSectorCount() * RegionValues.BYTES_PER_SECTOR - length - 5);
+
+			sectorsRead += loc.getSectorCount();
+
+			partitions.add(new Partition(PartitionType.CHUNK, chunk, loc.getSectorNumber(), loc.getSectorCount()));
+		}
 
 		return partitions;
 	}
@@ -38,20 +81,12 @@ public class RegionFileIO {
 			this.location = location;
 		}
 
-		public int getOffset() {
-			return offset;
-		}
-
 		public int getSectorNumber() {
 			return offset >> 8;
 		}
 
 		public int getSectorCount() {
 			return offset % 8;
-		}
-
-		public int getLocation() {
-			return location;
 		}
 
 		public int getX() {
