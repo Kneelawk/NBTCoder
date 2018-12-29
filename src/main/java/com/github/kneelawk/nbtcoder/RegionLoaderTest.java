88075b7441223608e3006420cd3f2a4a0a12ab16
Kneelawk <jedidiah.pommert@gmail.com>
2018-12-29 03:32:31 -0800

    Switch to new NBTFile parser system

--- src/main/java/com/github/kneelawk/nbtcoder/RegionLoaderTest.java
+++ src/main/java/com/github/kneelawk/nbtcoder/RegionLoaderTest.java
@@ -5,8 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import com.github.kneelawk.file.RegionNBTFile;
-import com.github.kneelawk.filelanguage.NBTFileLexer;
-import com.github.kneelawk.filelanguage.NBTFileParser;
+import com.github.kneelawk.filelanguage.NBTFileLanguageParser;
 import com.github.kneelawk.nbtlanguage.NBTLanguageParser;
 import com.github.kneelawk.region.RegionFileIO;
 
@@ -14,14 +13,15 @@ public class RegionLoaderTest {
 
 	public static void main(String[] args) throws IOException {
 		NBTLanguageParser nbtParser = new NBTLanguageParser();
-		NBTFileLexer lexer = new NBTFileLexer(new FileReader("../r.-1.-2.mca.txt"));
-		NBTFileParser parser = new NBTFileParser(lexer, nbtParser);
+		NBTFileLanguageParser parser = new NBTFileLanguageParser(nbtParser);
 
 		System.out.println("Parsing...");
-		RegionNBTFile file = (RegionNBTFile) parser.parseNBTFile();
+		RegionNBTFile file = (RegionNBTFile) parser.parse(new FileReader("../r.-1.-2.mca.txt"));
 
 		System.out.println("Writing...");
 		RegionFileIO.writeRegionFile(new FileOutputStream("../r.-1.-2.mca.2"), file.getPartitions());
+
+		System.out.println("Done.");
 	}
 
 }
