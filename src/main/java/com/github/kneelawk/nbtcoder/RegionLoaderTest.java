bcd6e340d391ac794a395c54312f60efcf00544a
Kneelawk <jedidiah.pommert@gmail.com>
2019-01-02 10:30:37 -0800

    Use NBTFileIO in the RegionLoaderTest

--- src/main/java/com/github/kneelawk/nbtcoder/RegionLoaderTest.java
+++ src/main/java/com/github/kneelawk/nbtcoder/RegionLoaderTest.java
@@ -4,10 +4,10 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
-import com.github.kneelawk.file.RegionNBTFile;
+import com.github.kneelawk.file.NBTFile;
+import com.github.kneelawk.file.NBTFileIO;
 import com.github.kneelawk.filelanguage.NBTFileLanguageParser;
 import com.github.kneelawk.nbtlanguage.NBTLanguageParser;
-import com.github.kneelawk.region.RegionFileIO;
 
 public class RegionLoaderTest {
 
@@ -16,10 +16,10 @@ public class RegionLoaderTest {
 		NBTFileLanguageParser parser = new NBTFileLanguageParser(nbtParser);
 
 		System.out.println("Parsing...");
-		RegionNBTFile file = (RegionNBTFile) parser.parse(new FileReader("../r.-1.-2.mca.txt"));
+		NBTFile file = parser.parse(new FileReader("../r.-1.-2.mca.txt"));
 
 		System.out.println("Writing...");
-		RegionFileIO.writeRegionFile(new FileOutputStream("../r.-1.-2.mca.2"), file.getPartitions());
+		NBTFileIO.writeNBTStream(file, new FileOutputStream("../r.-1.-2.mca.2"));
 
 		System.out.println("Done.");
 	}
