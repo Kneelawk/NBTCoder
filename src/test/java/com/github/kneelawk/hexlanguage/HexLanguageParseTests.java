41af7583455221cc87a7a22b9ce3826c89a3767a
Kneelawk <jedidiah.pommert@gmail.com>
2019-01-02 20:49:27 -0800

    Add hex language parse tests

--- src/test/java/com/github/kneelawk/hexlanguage/HexLanguageParseTests.java
+++ src/test/java/com/github/kneelawk/hexlanguage/HexLanguageParseTests.java
@@ -1,5 +1,25 @@
 package com.github.kneelawk.hexlanguage;
 
-public class HexLanguageParseTests {
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import com.github.kneelawk.test.ByteArrayArgumentConverter;
+
+public class HexLanguageParseTests {
+	@ParameterizedTest
+	@CsvFileSource(resources = { "printHexWithLineNumbersWithOffset.csv", "printHexWithoutLineNumbersWithOffset.csv",
+			"printHexWithLineNumbersWithoutOffset.csv", "printHexWithoutLineNumbersWithoutOffset.csv" })
+	public void parseHex(@ConvertWith(ByteArrayArgumentConverter.class) byte[] expected, int offset, String input)
+			throws IOException {
+		HexLanguageParser parser = new HexLanguageParser();
+
+		byte[] data = parser.parse(input);
+
+		assertArrayEquals(expected, data);
+	}
 }
